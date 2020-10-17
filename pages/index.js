2c80a17d6ca8cad8da97b88f5e325be115eb216d
lcc015u <ian825747@gmail.com>
2020-10-17 18:26:36 +0800

    fix iphone issue

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Index() {
       <>
       <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, 
-     user-scalable=no" />
+     user-scalable=no, minimum-scale=1.0" />
       </Head>
         < Menu/>
         <Container>
